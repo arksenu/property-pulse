1da6e15ad27de4764306b357e48d341bfd792c0b
Dario <leizaro.fei@gmail.com>
2025-11-05 19:05:28 -0800

    Add middleware, save/create user in database on sign

--- utils/authOptions.js
+++ utils/authOptions.js
@@ -1,3 +1,5 @@
+import User from "@/models/User";
+import connectDB from "@/config/database";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
@@ -18,15 +20,31 @@ export const authOptions = {
     // invoked on successful signin
     async signIn({ profile }) {
       // connect to DB
+      await connectDB();
       // check if the user exists
+      const userExists = await User.findOne({ email: profile.email });
       // if not, then add user to DB
+      if (!userExists) {
+        // truncate username in case its too long
+        const username = profile.name.slice(0, 20);
+
+        await User.create({
+          email: profile.email,
+          username,
+          image: profile.picture
+        });
+      }
       // return true to allow signin
+      return true;
     },
     // modifies the session object
     async session({ session }) {
       // get the user from DB
+      const user = await User.findOne({ email: session.user.email });
       // assign the user id  to the session
+      session.user.id = user._id.toString();
       // then return the session
+      return session;
     }
   }
 };
